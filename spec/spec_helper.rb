cb37a097f632b249077ca3bbac2ebb93feed6d87
Mikael Muszynski <linduxed@gmail.com>
2017-07-21 19:09:38 +0200

    Add Executable helper for specs
    The Executable provides a way to write acceptance tests for our add-on.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,41 @@
+require 'open3'
+
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
   config.order = 'random'
+end
+
+class Executable
+  def self.run(args_string = '')
+    new(args_string).tap(&:run)
+  end
+
+  def initialize(args_string)
+    @args_string = args_string
+  end
+
+  def run
+    _, @stdout, @stderr, @wait_thr = Open3.popen3(
+      "#{binary_location} #{@args_string}"
+    )
+  end
+
+  def lines
+    @lines ||= @stdout.readlines.map(&:chomp)
+  end
+
+  def error
+    @error ||= @stderr.read
+  end
+
+  def exit_code
+    @wait_thr.value.exitstatus
+  end
+
+  private
+
+  def binary_location
+    File.expand_path('../../bin/trello', __FILE__)
+  end
 end
