03d3f4f0e3061a466316ae00afdac7719e81d95f
Mikael Muszynski <linduxed@gmail.com>
2017-07-24 19:21:36 +0200

    Make Executable helper accept test fixtures
    To allow for testing the add-on with custom TODO_FILE instances, while
    not mocking anything internally, we need to pass a location for an
    alternate TODO_FILE through ENV variables.
    
    Extend the Executable helper and add another helper method
    (`with_todo_file`) to allow for creating a temporary TODO_FILE and
    injecting the path to it.
    
    As a minor note: the invocation of `gsub!` is there to allow for writing
    heredocs describing TODOs that are indented, instead of having to
    deindent them to the start of the line.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'open3'
+require 'tempfile'
 
 RSpec.configure do |config|
   config.run_all_when_everything_filtered = true
@@ -8,15 +9,19 @@ RSpec.configure do |config|
 end
 
 class Executable
-  def self.run(args_string = '')
-    new(args_string).tap(&:run)
+  def self.run(args_string = '', env_extension: {})
+    new(args_string, env_extension).tap(&:run)
   end
 
-  def initialize(args_string)
+  def initialize(args_string, env_extension)
     @args_string = args_string
+    @env_extension = env_extension
   end
 
   def run
+    old_env = ENV.to_h
+    ENV.update(@env_extension)
+
     # NOTE: The "padding" word in the following command is necessary because
     # regular invocations of the add-on will have all arguments placed on and
     # after ARGV[1]. For more information, read the comment in
@@ -24,6 +29,8 @@ class Executable
     command = "#{binary_location} padding #{@args_string}"
 
     _, @stdout, @stderr, @wait_thr = Open3.popen3(command)
+
+    ENV.replace(old_env)
   end
 
   def lines
@@ -43,4 +50,19 @@ class Executable
   def binary_location
     File.expand_path('../../bin/trello', __FILE__)
   end
+end
+
+def with_todo_file(todos)
+  todo_file = Tempfile.new('todo.txt')
+  todos.gsub!(/^\s+/, '')
+  todo_file.write(todos)
+  todo_file.rewind
+
+  env_extension = {
+    'TODO_FILE' => todo_file.path,
+  }
+
+  yield(todo_file, env_extension)
+
+  todo_file.delete
 end
