18c8c2300760a49a7236e216d62ea13cd037ced6
Mikael Muszynski <linduxed@gmail.com>
2017-10-03 18:19:54 +0200

    Refactor TodoFile#add_tag_to_todo and #find_todo
    Extract the notion of reading a specific TODO into a private method,
    since this is shared between TodoFile#add_tag_to_todo and
    TodoFile#find_todo.
    
    Additionally, extract a private method for writing to the TODO_FILE, for
    clarity's sake.
    
    Finally, it should be noted that this patch increases the amount of
    times the entire file is read with File.readlines. While accessing the
    disk unnecessarily bothers me somewhat, in this case the alternative
    would be to make the code more cluttered with file handling. For now,
    I'll run with the less performant option; I'll likely change this later.

--- lib/todo_file.rb
+++ lib/todo_file.rb
@@ -8,23 +8,30 @@ class TodoFile
   def add_tag_to_todo(todo_number:, trello_tag:)
     valid_todo_number?(todo_number) or raise BadTodoNumber
 
-    todos = File.readlines(@todo_file_path)
-    original_todo = todos[todo_number - 1]
-
+    original_todo = read_todo(number: todo_number)
     todo_with_tag = original_todo.chomp + " tr:#{trello_tag}" + "\n"
 
-    todos[todo_number - 1] = todo_with_tag
-    File.write(@todo_file_path, todos.join)
+    overwrite_todo(number: todo_number, new_todo: todo_with_tag)
   end
 
   def find_todo(number:)
     valid_todo_number?(number) or raise BadTodoNumber
 
-    todos = File.readlines(@todo_file_path)
-    todos[number - 1].chomp
+    read_todo(number: number)
   end
 
   private
+
+  def overwrite_todo(number:, new_todo:)
+    todos = File.readlines(@todo_file_path)
+    todos[number - 1] = new_todo
+    File.write(@todo_file_path, todos.join)
+  end
+
+  def read_todo(number:)
+    todos = File.readlines(@todo_file_path)
+    todos[number - 1].chomp
+  end
 
   def valid_todo_number?(number)
     todo_count = File.readlines(@todo_file_path).count
